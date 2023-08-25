2575bdee90956f16a8a1e4692be0a25b30f8c85c
rorychatt <pavel.rinne@gmail.com>
2023-08-25 17:09:38 +0300

    mysql init

---
+++ server/src/background/loadConfig.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+
+export interface DatabaseConfig {
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  waitForConnections: boolean;
+  connectionLimit: number;
+  queueLimit: number;
+}
+
+export interface SSHConfig {
+  password: string;
+  remoteIp: string;
+}
+
+export interface Config {
+  database: DatabaseConfig;
+  ssh: SSHConfig;
+}
+
+const readConfigFile = (filePath: string): Config | null => {
+  try {
+    const configFileContents = fs.readFileSync(filePath, "utf8");
+    const config = JSON.parse(configFileContents) as Config;
+    return config;
+  } catch (error) {
+    console.error("Error reading config file:", error.message);
+    return null;
+  }
+};
+
+export const config: Config = readConfigFile("./config.json");
