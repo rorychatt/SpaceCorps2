9ffbab17144f23261bfc0c98a41a2ca502ce1a31
rorychatt <pavel.rinne@gmail.com>
2023-08-26 13:24:01 +0300

    login works

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,7 +1,12 @@
 import * as mysql from "mysql2";
 import { config } from "../background/loadConfig";
 
-// Create a connection pool
+export interface UserCredentials {
+    username: string;
+    uuid: number;
+    password: string;
+    lastLogin: string;
+}
 
 export let pool: mysql.Pool;
 
@@ -22,46 +27,53 @@ export function setupDatabaseConnection(): void {
     });
 }
 
-export function getAllUsers(): Promise<unknown[]> {
+export function getAllUsers(): Promise<UserCredentials[]> {
   const query = "SELECT * FROM login";
   return executeQuery(query);
 }
 
-// Function to execute a query
-function executeQuery(query: string): Promise<unknown[]> {
-  return new Promise<unknown[]>((resolve, reject) => {
-    try {
-      pool.getConnection((connectionError, connection) => {
-        if (connectionError) {
-          console.error(
-            `Got error while connecting to DB, err = ${JSON.stringify(
-              connectionError
-            )}`
-          );
-          reject(connectionError);
-          return;
-        }
-        connection.query(query, (queryError, results) => {
-          connection.release(); // Release the connection
+export function getUserByUsername(username: string): Promise<UserCredentials[]> {
+    const query = `SELECT * FROM login WHERE username = "${username}";`
+    return executeQuery(query)
+}
 
-          if (queryError) {
+// Function to execute a query
+// Function to execute a query
+function executeQuery<T>(query: string): Promise<T[]> {
+    return new Promise<T[]>((resolve, reject) => {
+      try {
+        pool.getConnection((connectionError, connection) => {
+          if (connectionError) {
             console.error(
-              `Got error while querying DB, err = ${JSON.stringify(queryError)}`
+              `Got error while connecting to DB, err = ${JSON.stringify(
+                connectionError
+              )}`
             );
-            reject(queryError);
+            reject(connectionError);
             return;
           }
-
-          if (Array.isArray(results)) {
-            resolve(results);
-          } else {
-            // If the results are not an array, wrap them in an array
-            resolve([results]);
-          }
+          connection.query(query, (queryError, results) => {
+            connection.release(); // Release the connection
+  
+            if (queryError) {
+              console.error(
+                `Got error while querying DB, err = ${JSON.stringify(queryError)}`
+              );
+              reject(queryError);
+              return;
+            }
+  
+            if (Array.isArray(results)) {
+              resolve(results as T[]);
+            } else {
+              // If the results are not an array, wrap them in an array
+              resolve([results as T]);
+            }
+          });
         });
-      });
-    } catch (err) {
-      console.error(`Error while executing query = ${err}`);
-    }
-  });
-}
+      } catch (err) {
+        console.error(`Error while executing query = ${err}`);
+      }
+    });
+  }
+  
