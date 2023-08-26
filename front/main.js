de59bb5e6e5a07b32479c3085f8f1adefc910a57
rorychatt <pavel.rinne@gmail.com>
2023-08-26 15:20:47 +0300

    windows created

--- front/main.js
+++ front/main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron");
 const io = require("socket.io-client");
 
+let profileWindow, spacemapWindow
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -18,10 +20,24 @@ app.on("ready", () => {
   // Handle game-related events here
   // For example, socket.on('gameUpdate', handleGameUpdate);
 
+  socket.on("loginSuccess", () => {
+    profileWindow = new BrowserWindow({
+      parent: mainWindow,
+      show: false,
+    });
+    spacemapWindow = new BrowserWindow({ parent: mainWindow, show: false });
+    profileWindow.loadURL("http://localhost:3000/profileWindow.html");
+    spacemapWindow.loadURL("http://localhost:3000/spacemapWindow.html");
+    profileWindow.once('ready-to-show', () => {
+      profileWindow.show();
+    });
+    spacemapWindow.once('ready-to-show', () => {
+      spacemapWindow.show();
+    })
+  });
+
   mainWindow.on("closed", () => {
     // Disconnect from the server when the window is closed
     socket.disconnect();
   });
 });
-
-
