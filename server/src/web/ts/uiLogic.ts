fc46e156e4cfb5efc4cfa15439b16d40d80b1b49
dmitri.belov.db <dmitri.belov.db@gmail.com>
2023-09-08 19:33:46 +0300

    UIAlpha080923

--- server/src/web/ts/uiLogic.ts
+++ server/src/web/ts/uiLogic.ts
@@ -1,7 +1,5 @@
-const toggleMenuButton = document.getElementById("menu_btn") as HTMLElement;
+// Divs
 const mainMenuDiv = document.getElementById("main_menu_container") as HTMLElement;
-const returnToGameButton = document.getElementById("return_to_game_btn") as HTMLElement;
-const closeButton = document.getElementById("close_btn") as HTMLElement;
 const containerDiv = document.getElementById("container") as HTMLElement;
 const playerInfoDiv = document.getElementById("p_info") as HTMLElement;
 const clanInfoDiv = document.getElementById("c_info") as HTMLElement;
@@ -10,6 +8,16 @@ const auctionDiv = document.getElementById("auction") as HTMLElement;
 const assemblyDiv = document.getElementById("assembly") as HTMLElement;
 const quitDiv = document.getElementById("quit_container") as HTMLElement;
 const playerModalDiv = document.getElementById("player_info_modal") as HTMLElement;
+const shipModalDiv = document.getElementById("ship_info_modal") as HTMLElement;
+const chatModalDiv = document.getElementById("chat_info_modal") as HTMLElement;
+const spacemapModalDiv = document.getElementById("spacemap_info_modal") as HTMLElement;
+const logModalDiv = document.getElementById("log_info_modal") as HTMLElement;
+const assemblyModalDiv = document.getElementById("assembly_info_modal") as HTMLElement;
+
+// Buttons
+const returnToGameButton = document.getElementById("return_to_game_btn") as HTMLElement;
+const closeButton = document.getElementById("close_btn") as HTMLElement;
+const toggleMenuButton = document.getElementById("menu_btn") as HTMLElement;
 const playerInfoButton = document.getElementById("p_info_btn") as HTMLElement;
 const clanInfoButton = document.getElementById("clan_info_btn") as HTMLElement;
 const shopButton = document.getElementById("shop_btn") as HTMLElement;
@@ -20,6 +28,16 @@ const quitButtonYes = document.getElementById("quit_btn_yes") as HTMLElement;
 const quitButtonNo = document.getElementById("quit_btn_no") as HTMLElement;
 const playerModalButton = document.getElementById("player_modal_btn") as HTMLElement;
 const playerModalQuitButton = document.getElementById("modal_quit_btn") as HTMLElement;
+const shipModalButton = document.getElementById("ship_modal_btn") as HTMLElement;
+const shipModalQuitButton = document.getElementById("ship_modal_quit_btn") as HTMLElement;
+const chatModalButton = document.getElementById("chat_modal_btn") as HTMLElement;
+const chatModalQuitButton = document.getElementById("chat_modal_quit_btn") as HTMLElement;
+const spacemapModalButton = document.getElementById("spacemap_modal_btn") as HTMLElement;
+const spacemapModalQuitButton = document.getElementById("spacemap_modal_quit_btn") as HTMLElement;
+const logModalButton = document.getElementById("log_modal_btn") as HTMLElement;
+const logModalQuitButton = document.getElementById("log_modal_quit_btn") as HTMLElement;
+const assemblyModalButton = document.getElementById("assembly_modal_btn") as HTMLElement;
+const assemblyModalQuitButton = document.getElementById("assembly_modal_quit_btn") as HTMLElement;
 
 toggleMenuButton.addEventListener("click", function(): void {
     if (mainMenuDiv.style.display === "none" || mainMenuDiv.style.display === "") {
@@ -100,3 +118,100 @@ playerModalButton.addEventListener("click", function(): void {
 playerModalQuitButton.addEventListener("click", function(): void {
     playerModalDiv.style.display = "none";
 })
+
+shipModalButton.addEventListener("click", function() {
+    if(shipModalDiv.style.display === "none" || shipModalDiv.style.display === "") {
+        shipModalDiv.style.display = "block";
+    } else {
+        shipModalDiv.style.display = "none";
+    }
+})
+
+shipModalQuitButton.addEventListener("click", function() {
+    shipModalDiv.style.display = "none";
+})
+
+chatModalButton.addEventListener("click", function() {
+    if(chatModalDiv.style.display === "none" || chatModalDiv.style.display === "") {
+        chatModalDiv.style.display = "block";
+    } else {
+        chatModalDiv.style.display = "none";
+    }
+})
+
+chatModalQuitButton.addEventListener("click", function() {
+    chatModalDiv.style.display = "none";
+})
+
+spacemapModalButton.addEventListener("click", function() {
+    if(spacemapModalDiv.style.display === "none" || spacemapModalDiv.style.display === "") {
+        spacemapModalDiv.style.display = "block";
+    } else {
+        spacemapModalDiv.style.display = "none";
+    }
+})
+
+spacemapModalQuitButton.addEventListener("click", function() {
+    spacemapModalDiv.style.display = "none";
+})
+
+logModalButton.addEventListener("click", function() {
+    if(logModalDiv.style.display === "none" || logModalDiv.style.display === "") {
+        logModalDiv.style.display = "block";
+    } else {
+        logModalDiv.style.display = "none";
+    }
+})
+
+logModalQuitButton.addEventListener("click", function() {
+    logModalDiv.style.display = "none";
+})
+
+assemblyModalButton.addEventListener("click", function() {
+    if(assemblyModalDiv.style.display === "none" || assemblyModalDiv.style.display === "") {
+        assemblyModalDiv.style.display = "block";
+    } else {
+        assemblyModalDiv.style.display = "none";
+    }
+})
+
+assemblyModalQuitButton.addEventListener("click", function() {
+    assemblyModalDiv.style.display = "none";
+})
+
+// Movable DIVs
+const movableDivs = document.querySelectorAll(".movable-div");
+
+let offsetX: number, offsetY: number;
+let isDragging: boolean = false;
+let activeDiv: any = null;
+// TODO: peredalat huyny full pod 0 any sovsem kringe
+function onMouseDown(event:any) {
+    isDragging = true;
+    activeDiv = event.target;
+    offsetX = event.clientX - activeDiv.getBoundingClientRect().left;
+    offsetY = event.clientY - activeDiv.getBoundingClientRect().top;
+    activeDiv.style.cursor = "grabbing";
+}
+
+function onMouseMove(event:any) {
+    if (!isDragging) return;
+    activeDiv.style.left = event.clientX - offsetX + "px";
+    activeDiv.style.top = event.clientY - offsetY + "px";
+}
+
+function onMouseUp() {
+    if(activeDiv) {
+    isDragging = false;
+    activeDiv.style.cursor = "grab";
+    activeDiv = null;
+    }
+}
+
+movableDivs.forEach(div => {
+    div.addEventListener("mousedown", onMouseDown);
+});
+
+document.addEventListener("mousemove", onMouseMove);
+document.addEventListener("mouseup", onMouseUp);
+
