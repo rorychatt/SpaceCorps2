da4ba15a551106d7b8fca7ae8917fc54e274bf6d
Rostyslawn02 <rostyslawn02@gmail.com>
2023-09-25 12:37:35 +0300

    finished commands

--- server/src/server/background/ChatServer.ts
+++ server/src/server/background/ChatServer.ts
@@ -2,6 +2,8 @@ import { Server } from "socket.io";
 import { GameServer } from "./GameServer";
 import { gameServer } from "../main";
 import { Spacemap } from "./Spacemap";
+import { Entity } from "./Entity";
+import { Player } from "./Player";
 
 export class ChatServer {
     io: Server;
@@ -26,55 +28,47 @@ export class ChatServer {
         this.time = new Date();
         // TODO: check for privileges here!!!
 
-        /*
-        /i e ${uuid} : показывает информацию о конкретной сущности по ее uuid.
-
-        /i p ${name} : показывает информацию о конкретном игроке по его имени.
-
-        /c e ${name} : создать 1 объект по определенному имени на текущей карте игрока.
-
-        /d e ${uuid} : удалить объект по заданному uuid
-        */
-
         const message = consoleMessage.message.split(" ");
-        let entity;
 
-        // gameServer.spacemaps["map"].spawnAlien("1231");
         // to do command /d e, /c e
 
         switch(message[0]) {
             case "/i":
                 switch(message[1]) {
                     case "e":
-                        entity = await gameServer.getEntityByUUID(message[2]);
-                        console.log(entity);
+                        const entity = await gameServer.getEntityByUUID(message[2]);
+                        this._sendConsoleMessageToAll(JSON.stringify(entity));
                         break;
                     case "p":
-                        entity = await gameServer.getPlayerByUsername(message[2]);
-                        console.log(entity);
+                        const player = await gameServer.getPlayerByUsername(message[2]);
+                        this._sendConsoleMessageToAll(JSON.stringify(player));
                         break;
                 }
                 break;
             case "/c":
                 switch(message[1]) {
                     case "e": 
-                        gameServer.spacemaps["mapName"].spawnAlien(message[2], {
-                            x: (0.5 - Math.random()) * 10,
-                            y: (0.5 - Math.random()) * 10,
-                        });
+                        const player = await gameServer.getPlayerByUsername(consoleMessage.username);
+                        if(player?.currentMap) {
+                            gameServer.spacemaps[player.currentMap].spawnAlien(message[2], {
+                                x: (0.5 - Math.random()) * 10,
+                                y: (0.5 - Math.random()) * 10,
+                            });
+                        }
                         break;
                 }
                 break;
             case "/d":
                 switch(message[1]) {
                     case "e":
-                        gameServer.spacemaps["mapName"].deleteAlienByuuid(message[2]);
+                        const player = await gameServer.getPlayerByUsername(consoleMessage.username);
+                        if(player?.currentMap) {
+                            gameServer.spacemaps[player.currentMap].deleteAlienByuuid(message[2]);
+                        }
                         break;
                 }
                 break;
         }
-
-        this._sendConsoleMessageToAll(consoleMessage.message);
 
         // TODO: main logic
     }
