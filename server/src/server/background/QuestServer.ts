055fe7dac5878b5d1d948e250d60eb0b67274ccd
rorychatt <pavel.rinne@gmail.com>
2023-10-21 10:40:01 +0300

    json updates, amount in taskkill

--- server/src/server/background/QuestServer.ts
+++ server/src/server/background/QuestServer.ts
@@ -19,6 +19,7 @@ interface TaskFly {
 
 interface TaskKill {
     targetName: string;
+    amount: number;
     map: string;
     completed: boolean;
 }
@@ -65,7 +66,6 @@ export class Quest {
 }
 
 export class QuestServer {
-
     quests: Quest[] = [];
 
     constructor() {
@@ -86,72 +86,134 @@ export class QuestServer {
     async issueQuest(username: string, questName: string) {
         const player = await gameServer.getPlayerByUsername(username);
 
-        if(!player) return console.log(`Can't find player: ${player}`);
+        if (!player) return console.log(`Can't find player: ${player}`);
 
-        for(let i = 0; i < gameServer.questServer.quests.length; i++) {
-            for(let j = 0; j < player.currentActiveQuests.length; j++) if(player.currentActiveQuests[j].name == questName) return console.log(`Quest: ${questName} already active`);
-            if(player.currentActiveQuests.length >= maxQuestsPerPlayer) return console.log(`Max amount of quests per player: ${maxQuestsPerPlayer}`);
-            if(gameServer.questServer.quests[i].name == questName) player.addQuest(gameServer.questServer.quests[i]);
+        for (let i = 0; i < gameServer.questServer.quests.length; i++) {
+            for (let j = 0; j < player.currentActiveQuests.length; j++)
+                if (player.currentActiveQuests[j].name == questName)
+                    return console.log(`Quest: ${questName} already active`);
+            if (player.currentActiveQuests.length >= maxQuestsPerPlayer)
+                return console.log(
+                    `Max amount of quests per player: ${maxQuestsPerPlayer}`
+                );
+            if (gameServer.questServer.quests[i].name == questName)
+                player.addQuest(gameServer.questServer.quests[i]);
         }
     }
 
-    async registerOreCollection (data: { playerUUID: string, cargoDrop: CargoDrop }){
+    async registerOreCollection(data: {
+        playerUUID: string;
+        cargoDrop: CargoDrop;
+    }) {
         let player = await gameServer.getPlayerByUUID(data.playerUUID);
 
-        if(!player) return console.log(`Can't find player: ${player}`);
-        if(player.currentActiveQuests.length <= 0) return;
+        if (!player) return console.log(`Can't find player: ${player}`);
+        if (player.currentActiveQuests.length <= 0) return;
 
-        for(let i = 0; i < player.currentActiveQuests.length; i++) {
-            for(let j = 0; j < data.cargoDrop.ores.length; j++) {
-                for(let k = 0; k < player.currentActiveQuests[i].task.collect.length; k++) {
-                    if(data.cargoDrop.ores[i].name == player.currentActiveQuests[i].task.collect[k].oreName) {
-                        if(data.cargoDrop.ores[i].amount >= player.currentActiveQuests[i].task.collect[k].amount) {
-                            player.currentActiveQuests[i].task.collect[k].completed = true;
+        for (let i = 0; i < player.currentActiveQuests.length; i++) {
+            for (let j = 0; j < data.cargoDrop.ores.length; j++) {
+                for (
+                    let k = 0;
+                    k < player.currentActiveQuests[i].task.collect.length;
+                    k++
+                ) {
+                    if (
+                        data.cargoDrop.ores[i].name ==
+                        player.currentActiveQuests[i].task.collect[k].oreName
+                    ) {
+                        if (
+                            data.cargoDrop.ores[i].amount >=
+                            player.currentActiveQuests[i].task.collect[k].amount
+                        ) {
+                            player.currentActiveQuests[i].task.collect[
+                                k
+                            ].completed = true;
                         }
                     }
                 }
             }
 
-            this.checkForQuestComplete(player, player.currentActiveQuests[i].name);
+            this.checkForQuestComplete(
+                player,
+                player.currentActiveQuests[i].name
+            );
         }
     }
 
-    async registerAlienKill(data: { playerUUID: string, entityName: string }){
+    async registerAlienKill(data: { playerUUID: string; entityName: string }) {
         const player = await gameServer.getPlayerByUUID(data.playerUUID);
 
-        if(!player) return console.log(`Can't find player: ${player}`);
-        if(player.currentActiveQuests.length <= 0) return;
+        if (!player) return console.log(`Can't find player: ${player}`);
+        if (player.currentActiveQuests.length <= 0) return;
 
-        for(let i = 0; i < player.currentActiveQuests.length; i++) {
-            for(let j = 0; j < player.currentActiveQuests[i].task.kill.length; j++) {
-                if(!player.currentActiveQuests[i].task.kill) return;
-                if(player.currentActiveQuests[i].task.kill[j].targetName !== data.entityName) return;
+        for (let i = 0; i < player.currentActiveQuests.length; i++) {
+            for (
+                let j = 0;
+                j < player.currentActiveQuests[i].task.kill.length;
+                j++
+            ) {
+                if (!player.currentActiveQuests[i].task.kill) return;
+                if (
+                    player.currentActiveQuests[i].task.kill[j].targetName !==
+                    data.entityName
+                )
+                    return;
 
                 player.currentActiveQuests[i].task.kill[j].completed = true;
             }
-            
-            this.checkForQuestComplete(player, player.currentActiveQuests[i].name);
-        }   
+
+            this.checkForQuestComplete(
+                player,
+                player.currentActiveQuests[i].name
+            );
+        }
     }
 
     // доделать
-    async registerFlyDistance(data: {playerUUID: string, mapName: string, distanceTravelled: number}) {
-        
-    }
+    async registerFlyDistance(data: {
+        playerUUID: string;
+        mapName: string;
+        distanceTravelled: number;
+    }) {}
 
-    async checkForQuestComplete(player: Player, questName: string){
-        for(let i = 0; i < player.completedQuests.length; i++) if(player.completedQuests[i].questName == questName) return console.log(`Quest: ${questName} already completed!`);
+    async checkForQuestComplete(player: Player, questName: string) {
+        for (let i = 0; i < player.completedQuests.length; i++)
+            if (player.completedQuests[i].questName == questName)
+                return console.log(`Quest: ${questName} already completed!`);
 
-        for(let i = 0; i < player.currentActiveQuests.length; i++) {
-            if(player.currentActiveQuests[i].name == questName) {
-                if(player.currentActiveQuests[i].task.collect[0].completed == true && player.currentActiveQuests[i].task.kill[0].completed == true && player.currentActiveQuests[i].task.fly[0].completed == true) { 
+        for (let i = 0; i < player.currentActiveQuests.length; i++) {
+            if (player.currentActiveQuests[i].name == questName) {
+                if (
+                    player.currentActiveQuests[i].task.collect[0].completed ==
+                        true &&
+                    player.currentActiveQuests[i].task.kill[0].completed ==
+                        true &&
+                    player.currentActiveQuests[i].task.fly[0].completed == true
+                ) {
                     player.completeQuest(player.currentActiveQuests[i]);
                     player.currentActiveQuests.splice(i, 1);
 
-                    if(player.currentActiveQuests[i].reward.stats.credits) gameServer.rewardServer.registerCreditsReward(player.uuid, player.currentActiveQuests[i].reward.stats.credits);
-                    if(player.currentActiveQuests[i].reward.stats.thulium) gameServer.rewardServer.registerThuliumReward(player.uuid, player.currentActiveQuests[i].reward.stats.thulium);
-                    if(player.currentActiveQuests[i].reward.stats.experience) gameServer.rewardServer.registerExperienceReward(player.uuid, player.currentActiveQuests[i].reward.stats.experience);     
-                    if(player.currentActiveQuests[i].reward.stats.honor) gameServer.rewardServer.registerHonorReward(player.uuid, player.currentActiveQuests[i].reward.stats.honor);
+                    if (player.currentActiveQuests[i].reward.stats.credits)
+                        gameServer.rewardServer.registerCreditsReward(
+                            player.uuid,
+                            player.currentActiveQuests[i].reward.stats.credits
+                        );
+                    if (player.currentActiveQuests[i].reward.stats.thulium)
+                        gameServer.rewardServer.registerThuliumReward(
+                            player.uuid,
+                            player.currentActiveQuests[i].reward.stats.thulium
+                        );
+                    if (player.currentActiveQuests[i].reward.stats.experience)
+                        gameServer.rewardServer.registerExperienceReward(
+                            player.uuid,
+                            player.currentActiveQuests[i].reward.stats
+                                .experience
+                        );
+                    if (player.currentActiveQuests[i].reward.stats.honor)
+                        gameServer.rewardServer.registerHonorReward(
+                            player.uuid,
+                            player.currentActiveQuests[i].reward.stats.honor
+                        );
                 }
             }
         }
